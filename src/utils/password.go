1208a5d95fcbd158a286c6c7ba956bb07836f3b8
Mauro Maia <maurofilipemaia@gmail.com>
2021-10-03 18:22:18 +0100

    fix(core):Allow passwords with len >= 6 to allow 6 pin digits (this will change in the future)

--- src/utils/password.go
+++ src/utils/password.go
@@ -3,8 +3,8 @@ package utils
 import "errors"
 
 func ValidatePassword(password string) error {
-	if len(password) < 8 {
-		return errors.New("Invalid Password len. Must be >= 8")
+	if len(password) < 6 {
+		return errors.New("Invalid Password len. Must be >= 6")
 	}
 	return nil
 }
