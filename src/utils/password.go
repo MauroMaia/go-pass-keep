0531657cfe0b612bc36934ea48f1ca8cfdea7404
Mauro Maia <mauro.maia@g.xarevision.pt>
2021-10-03 01:14:00 +0100

    fix(core):ValidatePassword wrong check size

--- src/utils/password.go
+++ src/utils/password.go
@@ -3,7 +3,7 @@ package utils
 import "errors"
 
 func ValidatePassword(password string) error {
-	if len(password) > 8 {
+	if len(password) < 8 {
 		return errors.New("Invalid Password len. Must be >= 8")
 	}
 	return nil
